f6f14564868037894125071265d3254740ba71fb
Camvan955 <nguyenvann955@gmail.com>
2022-09-06 15:56:26 +0700

    nop bai tap

--- src/ss1_introduce_to_java/exercise/ReadNumber.java
+++ src/ss1_introduce_to_java/exercise/ReadNumber.java
@@ -8,7 +8,7 @@ public class ReadNumber {
         System.out.println("nhap so can doi: ");
         int number = scanner.nextInt();
 
-        if (number <= 0 & number < 10) {
+        if (number >= 0 & number < 10) {
             switch (number) {
                 case 0:
                     System.out.print("zero");
@@ -163,36 +163,124 @@ public class ReadNumber {
                     System.out.print("nine hundred");
                     break;
             }
-            switch (number /10) {
-                case 1:
-                    System.out.print("");
-                case 2:
-                    System.out.print("twenty");
-                    break;
-                case 3:
-                    System.out.print("thirty");
-                    break;
-                case 4:
-                    System.out.print("forty");
-                    break;
-                case 5:
-                    System.out.print("fifty");
-                    break;
-                case 6:
-                    System.out.print("sixty");
-                    break;
-                case 7:
-                    System.out.print("seventy");
-                    break;
-                case 8:
-                    System.out.print("eighty");
-                    break;
-                case 9:
-                    System.out.print("ninety");
-                    break;
-
+            if ((number % 100) > 0 && (number % 100) < 20) {
+                switch (number % 100) {
+                    case 1:
+                        System.out.print(" and one");
+                        break;
+                    case 2:
+                        System.out.print(" and two");
+                        break;
+                    case 3:
+                        System.out.print(" and three");
+                        break;
+                    case 4:
+                        System.out.print(" and four");
+                        break;
+                    case 5:
+                        System.out.print(" and five");
+                        break;
+                    case 6:
+                        System.out.print(" and six");
+                        break;
+                    case 7:
+                        System.out.print(" and seven");
+                        break;
+                    case 8:
+                        System.out.print(" and eight");
+                        break;
+                    case 9:
+                        System.out.print(" and nine");
+                        break;
+                    case 10:
+                        System.out.print(" and ten");
+                        break;
+                    case 11:
+                        System.out.print(" and eleven");
+                        break;
+                    case 12:
+                        System.out.print(" and twelve");
+                        break;
+                    case 13:
+                        System.out.print(" thirteen");
+                        break;
+                    case 14:
+                        System.out.print(" fourteen");
+                        break;
+                    case 15:
+                        System.out.print(" fifteen");
+                        break;
+                    case 16:
+                        System.out.print(" sixteen");
+                        break;
+                    case 17:
+                        System.out.print(" seventeen");
+                        break;
+                    case 18:
+                        System.out.print(" eighteen");
+                        break;
+                    case 19:
+                        System.out.print(" ninteen");
+                        break;
+                }
+            } else {
+                switch ((number / 10) % 10) {
+                    case 2:
+                        System.out.print(" twenty");
+                        break;
+                    case 3:
+                        System.out.print(" thirty");
+                        break;
+                    case 4:
+                        System.out.print(" forty");
+                        break;
+                    case 5:
+                        System.out.print(" fifty");
+                        break;
+                    case 6:
+                        System.out.print(" sixty");
+                        break;
+                    case 7:
+                        System.out.print(" seventy");
+                        break;
+                    case 8:
+                        System.out.print(" eighty");
+                        break;
+                    case 9:
+                        System.out.print(" ninety");
+                        break;
+                }
+                switch ((number % 100) % 10) {
+                    case 1:
+                        System.out.println(" one");
+                    case 2:
+                        System.out.print(" two");
+                        break;
+                    case 3:
+                        System.out.print(" three");
+                        break;
+                    case 4:
+                        System.out.print(" four");
+                        break;
+                    case 5:
+                        System.out.print(" five");
+                        break;
+                    case 6:
+                        System.out.print(" six");
+                        break;
+                    case 7:
+                        System.out.print(" seven");
+                        break;
+                    case 8:
+                        System.out.print(" eight");
+                        break;
+                    case 9:
+                        System.out.print(" nine");
+                        break;
+                }
             }
+        } else {
+            System.out.println("So nhap vao khong hop le!");
         }
     }
 }
-
