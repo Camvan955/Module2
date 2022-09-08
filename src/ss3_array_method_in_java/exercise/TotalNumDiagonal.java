ec309e67d8afa6f00f95c8e6fe74e83c66b1dd60
Camvan955 <nguyenvann955@gmail.com>
2022-09-08 22:18:56 +0700

    nop bai tap

--- src/ss3_array_method_in_java/exercise/TotalNumDiagonal.java
+++ src/ss3_array_method_in_java/exercise/TotalNumDiagonal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TotalNumDiagonal {
 
     //tính tổng các số trong đường chéo chính với mảng có sẵn
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 //        int[][] array = {{1, 2, 12}, {4, 5, 6}, {7, 8, 9}};
 //        int sum = 0;
 //
@@ -16,37 +16,37 @@ public class TotalNumDiagonal {
 //    }
 
 
-    //tính tổng các số trong đường chéo chính với mảng nhập vào từ bàn phím
+        //tính tổng các số trong đường chéo chính với mảng nhập vào từ bàn phím
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập vào số dòng của mảng: ");
-    int numberRow = Integer.parseInt(scanner.nextLine());
-       System.out.print("Nhập vào số cột của mảng: ");
-       int numberColumn;
+        int numberRow = Integer.parseInt(scanner.nextLine());
+        System.out.print("Nhập vào số cột của mảng: ");
+        int numberColumn;
         do {
             numberColumn = Integer.parseInt(scanner.nextLine());
-            if(numberColumn != numberRow){
+            if (numberColumn != numberRow) {
                 System.out.println("Nhập lại số cột bằng số dòng: ");
             }
-        }while (numberColumn != numberRow);
+        } while (numberColumn != numberRow);
 
-    int[][] array = new int[numberRow][numberColumn];
+        int[][] array = new int[numberRow][numberColumn];
 
 
         for (int i = 0; i < numberRow; i++) {
-        for (int j = 0; j < numberColumn; j++) {
-            System.out.print("Nhập phần tử thứ [" + i + ", " + j + "]: ");
-            array[i][j] = scanner.nextInt();
+            for (int j = 0; j < numberColumn; j++) {
+                System.out.print("Nhập phần tử thứ [" + i + ", " + j + "]: ");
+                array[i][j] = scanner.nextInt();
+            }
         }
-    }
 
 
         System.out.println("\nMảng vừa nhập: ");
         for (int i = 0; i < numberRow; i++) {
-        for (int j = 0; j < numberColumn; j++) {
-            System.out.print(array[i][j] + "\t");
+            for (int j = 0; j < numberColumn; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
         }
-    }
 
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
